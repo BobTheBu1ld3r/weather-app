45eea693b275c2d1d7cd71238d5df5b63c0d1789
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-08-17 15:48:32 +0100

    Export ScreenController using main.js

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-import renderSearchSuggestions from "./searchSuggestion.js";
 import FetchController from "./FetchController.js";
 const fetchController = new FetchController();
+import DataInterfacer from "./DataInterfacer.js";
+const dataInterfacer = new DataInterfacer();
+import ScreenController from "./ScreenController.js";
+const screenController = new ScreenController();
 
 const searchInput = document.querySelector("input#search");
 
 searchInput.addEventListener("input", async function handleSearch() {
-  resetSearchSuggestions();
+  screenController.resetSearchSuggestions();
   const searchString = searchInput.value;
   if (!searchString) return;
   const data = await fetchController.fetchSearchSuggestions(searchString);
-  renderSearchSuggestions(data);
+  const parsedData = dataInterfacer.suggestionDataParser(data);
+  screenController.renderSearchSuggestions(parsedData);
 });
 
-function resetSearchSuggestions() {
-  const suggestionContainer = document.querySelector(".suggestion-container");
-  suggestionContainer.innerHTML = "";
-}
+export default screenController;
