697d730537d5463e1dd1ab7472202be53d12327a
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-08-17 14:03:26 +0100

    Store API_KEY and url string in the FetchControlle

--- FetchController.js
+++ FetchController.js
@@ -1,7 +1,14 @@
 export default class FetchController {
-  async fetchSearchSuggestions(url) {
+  constructor() {
+    this.API_KEY = "ff89deb859334f57bbb115524233107";
+  }
+
+  async fetchSearchSuggestions(searchString) {
+    const url = `http://api.weatherapi.com/v1/search.json?key=${this.API_KEY}&q=${searchString}`;
     const response = await fetch(url);
     const data = await response.json();
     return data;
   }
+
+  async fetchForecast(locationID) {}
 }
