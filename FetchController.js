004d6d3406f638d44bfbe4d40fa28477cdbe9fc7
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-08-19 18:29:17 +0100

    Change protocol from http to https

--- FetchController.js
+++ FetchController.js
@@ -5,14 +5,14 @@ export default class FetchController {
   }
 
   async fetchSearchSuggestions(searchString) {
-    const url = `http://api.weatherapi.com/v1/search.json?key=${this.API_KEY}&q=${searchString}`;
+    const url = `https://api.weatherapi.com/v1/search.json?key=${this.API_KEY}&q=${searchString}`;
     const response = await fetch(url, { mode: "cors" });
     const data = await response.json();
     return data;
   }
 
   async fetchForecast(locationUrl) {
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${locationUrl}&days=${this.FORECAST_DAYS}&aqi=no&alerts=no
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${locationUrl}&days=${this.FORECAST_DAYS}&aqi=no&alerts=no
     `;
     const response = await fetch(url, { mode: "cors" });
     const data = await response.json();
