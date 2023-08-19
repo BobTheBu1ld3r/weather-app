f549c9aa7ac2f6b4acccae3b56005b79dfb344cc
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-08-19 17:31:01 +0100

    Delete unused files

--- fetchController.js
+++
@@ -1,7 +0,0 @@
-export default class FetchController {
-  async fetchSearchSuggestions(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  }
-}
