4f955de7821541559155d51136c2c15ae39c2220
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-08-19 18:27:25 +0100

    Add {mode : "cors"}

--- FetchController.js
+++ FetchController.js
@@ -6,7 +6,7 @@ export default class FetchController {
 
   async fetchSearchSuggestions(searchString) {
     const url = `http://api.weatherapi.com/v1/search.json?key=${this.API_KEY}&q=${searchString}`;
-    const response = await fetch(url);
+    const response = await fetch(url, { mode: "cors" });
     const data = await response.json();
     return data;
   }
@@ -14,7 +14,7 @@ export default class FetchController {
   async fetchForecast(locationUrl) {
     const url = `http://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${locationUrl}&days=${this.FORECAST_DAYS}&aqi=no&alerts=no
     `;
-    const response = await fetch(url);
+    const response = await fetch(url, { mode: "cors" });
     const data = await response.json();
     return data;
   }
