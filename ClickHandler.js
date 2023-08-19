869e2c47418ae3dfe7182e56c404c2d0e2207fe8
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-08-19 10:45:54 +0100

    Update handleLocationSelection

--- ClickHandler.js
+++ ClickHandler.js
@@ -1,11 +1,16 @@
-import screenController from "./main.js";
+import { fetchController, dataInterfacer, screenController } from "./main.js";
 
 export default class ClickHandler {
-  handleLocationSelection(event) {
+  async handleLocationSelection(event) {
     const url = event.target.dataset.url;
     const infoString = event.target.dataset.infoString;
     const input = document.querySelector("input#search");
     input.value = infoString;
     screenController.resetSearchSuggestions();
+    const data = await fetchController.fetchForecast(url);
+    const parsedData = dataInterfacer.dailyForecastParser(data);
+    screenController.renderDailyInfo(parsedData);
   }
+
+  handleDaySelection(event) {}
 }
